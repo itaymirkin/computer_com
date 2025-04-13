0301bd424755b5195cc320aa098420c6fd633dbd
Itay Mirkin <itaymirkin@mail.tau.ac.il>
2025-04-13 17:47:50 +0300

    Working. Need to build test for collision

--- channel.c
+++ channel.c
@@ -8,192 +8,238 @@
 #pragma comment(lib, "Ws2_32.lib")
 
 int send_to_all_sockets(fd_set* sock_set, char* packet, int frame_size) {
-	for (int i = 1; i < sock_set->fd_count; i++) {
-		SOCKET sock = sock_set->fd_array[i];
-			if (send(sock, packet, frame_size, 0) == SOCKET_ERROR) {
-				printf("send() failed: %d\n", WSAGetLastError());
-				return -1;
-		}
-	}
-	return 0;
+    for (int i = 1; i < sock_set->fd_count; i++) {
+        SOCKET sock = sock_set->fd_array[i];
+        if (send(sock, packet, frame_size, 0) == SOCKET_ERROR) {
+            printf("send() failed: %d\n", WSAGetLastError());
+            return -1;
+        }
+    }
+    return 0;
 }
 
-// TODO: 
-// 1. Ask how to handle errors
-// 2. MAX sizez
-// 3. Ask about timeout behavior, do we need to use select or sleep
-
 int main(int argc, char* argv[]) {
+    printf("Server starting...\n");
 
-	//Check number of params
-	if (argc != 3) {
-		fprintf(stderr, "Not enough parameters - need 3");
-		return 1;
-	}
+    // Init Winsock
+    WSADATA wsaData;
+    int wsaStartupResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (wsaStartupResult != 0) {
+        printf("WSAStartup failed: %d\n", wsaStartupResult);
+        return 1;
+    }
 
-	int chan_port = argv[0];
-	int slot_time = argv[1];
-	
-	char user_input[100];
+    // Check number of params
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <port> <slot_time_ms>\n", argv[0]);
+        WSACleanup();
+        return 1;
+    }
 
-	fd_set sock_set;
-	FD_ZERO(&sock_set);
+    int chan_port = atoi(argv[1]);
+    int slot_time = atoi(argv[2]);
 
-	int conflict		  = 0;
-	int sending_id		  = 0;
-	int server_is_sending = 0;
+    char user_input[100];
 
-	SOCKET client_sock;
+    fd_set sock_set;
+    FD_ZERO(&sock_set);
 
-	struct timeval timeout;
-	timeout.tv_sec = slot_time * 0.001;
+    int conflict = 0;
+    int sending_id = 0;
+    int server_is_sending = 0;
 
-	channel_stats_t clients_statistics[MAX_CLIENTS] = { 0 };
+    SOCKET client_sock;
 
-	// Create Socket 
-	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (listen_sock < 0) {
-		perror("socket");
-		return 1;
-	}
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = slot_time * 1000; 
 
+    channel_stats_t clients_statistics[MAX_CLIENTS] = { 0 };
 
-	struct sockaddr_in server_addr = { 0 };
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;      
-	server_addr.sin_port = htons(chan_port);
+    // Create Socket 
+    SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock == INVALID_SOCKET) {
+        printf("socket() failed: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+    printf("Socket created.\n");
 
-	if (bind(listen_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) { // Bind socket to port
-		perror("bind");
-		close(listen_sock);
-		return 1;
-	}
+    struct sockaddr_in server_addr = { 0 };
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(chan_port);
 
-	FD_SET(listen_sock, &sock_set); // Add the listening socket to the set
+    if (bind(listen_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) { // Bind socket to port
+        printf("bind() failed: %d\n", WSAGetLastError());
+        closesocket(listen_sock);
+        WSACleanup();
+        return 1;
+    }
+    printf("Socket bound to port %d.\n", chan_port);
 
-	if (listen(listen_sock, MAX_CLIENTS) < 0) { // Listen for incoming connections
-		perror("listen");
-		close(listen_sock);
-		return 1;
-	}
+    FD_SET(listen_sock, &sock_set); //listening socket to the set
 
-	SOCKET active_sock = accept(listen_sock, NULL, NULL); // Accept a connection
+    if (listen(listen_sock, MAX_CLIENTS) == SOCKET_ERROR) { // listen for incoming connections
+        printf("listen() failed: %d\n", WSAGetLastError());
+        closesocket(listen_sock);
+        WSACleanup();
+        return 1;
+    }
+    printf("Listening for incoming connections...\n");
 
-	if (active_sock < 0) {
-		perror("accept");
-		close(listen_sock);
-		return 1;
-	}
-
-	FD_SET(active_sock, &sock_set); // Add the active socket to the set
-
-
-	while (1) 
-	{
-		if (fgets(user_input, sizeof(user_input), stdin) == NULL) {
-			// This happens when Ctrl+Z is pressed
-			break;
-		}
-
-		server_is_sending = 0;
-		conflict = 0;
-
-		int result = select(0, &sock_set, NULL, NULL, NULL); // Wait for activity on the socket (need to add timeout)
-		if (result < 0) {
-			perror("select");
-			break;
-		}
-
-		else if (result == 0) continue;
-
-		if (FD_ISSET(listen_sock, &sock_set)) { // New connection
-			SOCKET new_sock = accept(listen_sock, NULL, NULL);
-			if (new_sock < 0) {
-				perror("accept");
-				break;
-			}
-			FD_SET(new_sock, &sock_set); // Add the new socket to the set
-		}
-
-		for (int i = 1; i < sock_set.fd_count; i++) 
-		{
-			if (FD_ISSET(sock_set.fd_array[i], &sock_set)) 
-			{
-				if (server_is_sending)
-				{
-					conflict = 1;
-					clients_statistics[sending_id].collisions++; // Increment collision count for the previous collision found
-				}
-				sending_id = i;
-				server_is_sending = 1;
-				client_sock = sock_set.fd_array[i];
-			}
-		}
-
-		if (conflict)
-		{
-			clients_statistics[sending_id].collisions++;
-			packet_header_t* packet = { 0 };
-			packet->type = PACKET_TYPE_NOISE;
-			if (send_to_all_sockets(&sock_set, (char*)packet, HEADER_SIZE) < 0)
-			{
-				perror("send");
-				break;
-			}
-		}
-		else if (server_is_sending)
-		{
-			char packet[MAX_FRAME_SIZE];
-			int packet_size = recv(client_sock, packet, sizeof(packet), 0);
-
-			// Get the packet header
-			packet_header_t* packet_header = (packet_header_t*)packet;
-
-			// Update the client's statistics
-			clients_statistics[sending_id].src_port = packet_header->src_port;
-
-			struct in_addr src;
-			src.s_addr = packet_header->src_ip; // Extract the IP address from the packet header
-
-			inet_ntop(AF_INET, &src, clients_statistics[sending_id].src_ip, 16);
-			clients_statistics[sending_id].sent++;
-
-			if (packet_size < 0) {
-				perror("recv");
-				break;
-			}
-			else if (packet_size < HEADER_SIZE) { // Incomplete transmission
-				fprintf(stderr, "Incomplete packet received\n");
-				packet_header_t* packet = { 0 };
-				packet->type = PACKET_TYPE_NOISE;
-				packet_size = HEADER_SIZE; // Change the packet to only the size of the header
-			}
-
-			if (send_to_all_sockets(&sock_set, (char*)packet, packet_size) < 0)
-			{
-				perror("send");
-				break;
-			}
-		}
-	}
+    
+    while (1) {
 
 
-	// Clean up
-	for (int i = 0; i < sock_set.fd_count; i++) {
-		closesocket(sock_set.fd_array[i]);
-	}
+        server_is_sending = 0;
+        conflict = 0;
 
-	// Print Statistics
-	for (int i = 0; i < MAX_CLIENTS; i++) {
-		if (clients_statistics[i].sent > 0) {
-			fprintf(stderr, "From %s, Port: %d: %d frames, %d collisions\n",
-				clients_statistics[i].src_ip, clients_statistics[i].src_port,
-				clients_statistics[i].sent, clients_statistics[i].collisions);
-		}
-	}
+        fd_set read_fds;
+        FD_ZERO(&read_fds);
+        read_fds = sock_set;  // copy the original set
 
+        // Set timeout for each iteration
+        struct timeval current_timeout = timeout;
 
-	WSACleanup();
+        int result = select(0, &read_fds, NULL, NULL, &current_timeout); // Wait for activity on the socket
+        if (result == SOCKET_ERROR) {
+            printf("select() failed: %d\n", WSAGetLastError());
+            break;
+        }
+        else if (result == 0) {
+            // Timeout occurred
+            continue;
+        }
 
-	return 0;
+        // Check if a new connection is available
+        if (FD_ISSET(listen_sock, &read_fds)) {
+            SOCKET new_sock = accept(listen_sock, NULL, NULL);
+            if (new_sock == INVALID_SOCKET) {
+                printf("accept() failed: %d\n", WSAGetLastError());
+                continue; 
+            }
+            FD_SET(new_sock, &sock_set); // Add the new socket to the set
+            printf("New connection accepted. Total connections: %d\n", sock_set.fd_count - 1);
+        }
+
+        // Check for data on client sockets
+        for (int i = 1; i < sock_set.fd_count; i++) {
+            SOCKET current_sock = sock_set.fd_array[i];
+
+            // Skip the listening socket
+            if (current_sock == listen_sock) continue;
+
+            if (FD_ISSET(current_sock, &read_fds)) {
+                if (server_is_sending) {
+                    conflict = 1;
+                    clients_statistics[sending_id].collisions++; // Increment collision count for the previous collision found
+                    printf("Conflict detected between clients %d and %d\n", sending_id, i);
+                }
+                sending_id = i;
+                server_is_sending = 1;
+                client_sock = current_sock;
+            }
+        }
+
+        if (conflict) {
+            clients_statistics[sending_id].collisions++;
+            packet_header_t packet = { 0 };
+            packet.type = PACKET_TYPE_NOISE;
+
+            if (send_to_all_sockets(&sock_set, (char*)&packet, HEADER_SIZE) < 0) {
+                printf("Failed to send noise packet\n");
+                
+            }
+            else {
+                printf("Conflict detected, noise packet sent.\n");
+            }
+        }
+        else if (server_is_sending) {
+            char packet[MAX_FRAME_SIZE];
+            int packet_size = recv(client_sock, packet, sizeof(packet), 0);
+
+            if (packet_size == SOCKET_ERROR) {
+                int error = WSAGetLastError();
+                if (error == WSAECONNRESET || error == WSAECONNABORTED) {
+                    printf("Client disconnected unexpectedly\n");
+                    closesocket(client_sock);
+                    FD_CLR(client_sock, &sock_set);
+                }
+                else {
+                    printf("recv() failed: %d\n", error);
+                }
+                continue;
+            }
+            else if (packet_size == 0) {
+                //client disconnect
+                printf("Client %d disconnected\n", sending_id);
+                closesocket(client_sock);
+                FD_CLR(client_sock, &sock_set);
+                continue;
+            }
+            else if (packet_size < HEADER_SIZE) {
+                fprintf(stderr, "Incomplete packet received (size: %d)\n", packet_size);
+                packet_header_t noise_packet = { 0 };
+                noise_packet.type = PACKET_TYPE_NOISE;
+
+                if (send_to_all_sockets(&sock_set, (char*)&noise_packet, HEADER_SIZE) < 0) {
+                    printf("Failed to send noise packet for incomplete transmission\n");
+                }
+                continue;
+            }
+
+            // Get the packet header
+            packet_header_t* packet_header = (packet_header_t*)packet;
+
+            // Update the client's statistics
+            clients_statistics[sending_id].src_port = packet_header->src_port;
+
+            struct in_addr src;
+            src.s_addr = packet_header->src_ip; // Extract the IP address from the packet header
+
+            inet_ntop(AF_INET, &src, clients_statistics[sending_id].src_ip, 16);
+            clients_statistics[sending_id].sent++;
+
+            printf("Received packet of size %d from client %d (IP: %s, Port: %d)\n",
+                packet_size, sending_id, clients_statistics[sending_id].src_ip,
+                clients_statistics[sending_id].src_port);
+
+            if (send_to_all_sockets(&sock_set, packet, packet_size) < 0) {
+                printf("Failed to forward packet to all clients\n");
+                // Just continue, don't break the loop
+            }
+            else {
+                printf("Packet successfully forwarded to all clients\n");
+            }
+        }
+    }
+
+    // Clean up
+    for (int i = 0; i < sock_set.fd_count; i++) {
+        closesocket(sock_set.fd_array[i]);
+    }
+
+    // Print Statistics
+    printf("\n========== Channel Statistics ==========\n");
+    int active_clients = 0;
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients_statistics[i].sent > 0) {
+            active_clients++;
+            printf("Client %d - IP: %s, Port: %d: %d frames, %d collisions\n",
+                i, clients_statistics[i].src_ip, clients_statistics[i].src_port,
+                clients_statistics[i].sent, clients_statistics[i].collisions);
+        }
+    }
+
+    if (active_clients == 0) {
+        printf("No active clients recorded\n");
+    }
+
+    printf("========================================\n");
+
+    WSACleanup();
+    printf("Server shutting down...\n");
+
+    return 0;
 }
--- server.c
+++ server.c
@@ -15,276 +15,271 @@ int total_frames = 0;
 int total_transmissions = 0;
 int max_transmissions = 0;
 
-
 typedef struct {
-	int success;
-	int nof_retransmit;
+    int success;
+    int nof_retransmit;
 } send_st_t;
 
-//send fucntion
-
+//send function
 send_st_t send_func(int socket, const void* data, size_t frame_size, int seq_num, int slot_time, int timeout, struct sockaddr_in* chan_addr) {
+    send_st_t status = { 0,0 };
+    int max_attempts = 10;
+    int nof_failures = 0;
 
-	send_st_t status = { 0,0 };
-	int max_attempts = 10;
-	int nof_failures = 0;
+    //create packet
+    char* packet = (char*)malloc(frame_size);
+    if (packet == NULL) {
+        perror("Failed to allocate memory for packet");
+        exit(1);
+    }
+    printf("Packet created successfully.\n");
+    // Create a pointer to the packet header
+    packet_header_t* header = (packet_header_t*)packet;
 
-	//create packet
+    //fill header attributes
+    header->type = PACKET_TYPE_DATA;
+    header->seq_num = htonl(seq_num);
+    header->src_ip = chan_addr->sin_addr.s_addr;
+    header->src_port = chan_addr->sin_port;
+    header->length = htons((uint16_t)(frame_size - HEADER_SIZE));
+    printf("Header created successfully.\n");
+    //cp data for rest of packet
+    memcpy(packet + HEADER_SIZE, data, frame_size - HEADER_SIZE);
 
-	char* packet = (char*)malloc(frame_size);
-	if (packet == NULL) {
-		perror("Failed to allocate memory for packet");
-		exit(1); 
-	}
+    LARGE_INTEGER start_time, end_time;
+    QueryPerformanceCounter(&start_time);
 
-	// Create a pointer to the packet header
-	packet_header_t* header = (packet_header_t*)packet;
+    while (nof_failures < max_attempts) {
+        //try_sending
+        if (send(socket, packet, frame_size, 0) < 0) {
+            perror("send");
+            OutputDebugString("Send failed\n");
+            return status;
+        }
+        
+        status.nof_retransmit++;
+        printf("Packet sent %d times\n", status.nof_retransmit);
+        OutputDebugString("Packet sent\n");
 
-	//fill header attributes
-	
-	header->type = PACKET_TYPE_DATA;
-	header->seq_num = htonl(seq_num);
-	header->src_ip = chan_addr->sin_addr.s_addr;
-	header->src_port = chan_addr->sin_port;
-	header->length = htons((uint16_t)(frame_size - HEADER_SIZE));
+        //check if sent
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(socket, &readfds);
 
-	//cp data for rest of packet
-	memcpy(packet + HEADER_SIZE, data, frame_size - HEADER_SIZE);
+        struct timeval tv;
+        tv.tv_sec = timeout;
+        tv.tv_usec = 0;
 
-	LARGE_INTEGER start_time, end_time;
-	QueryPerformanceCounter(&start_time);
+        int select_resp = select(socket + 1, &readfds, NULL, NULL, &tv);
+        printf("Select response: %d\n", select_resp);
+        //error
+        if (select_resp < 0) {
+            perror("select");
+            OutputDebugString("Select failed\n");
+            return status;
+        }
+        //timeout
+        else if (select_resp == 0) {
+            nof_failures++;
+            OutputDebugString("Timeout occurred\n");
+            Sleep(rand() % (1 << nof_failures) * slot_time); //exp backoff
+            continue;
+        }
 
-	while (nof_failures < max_attempts) {
-		//try_sending
-		if (send(socket, packet, frame_size, 0) < 0) {
-			perror("send");
-			return status;
-		}
+        //wait for ack
+        char* recv_packet = (char*)malloc(frame_size);
+        if (recv_packet == NULL) {
+            perror("Failed to allocate memory for recv_packet");
+            exit(1);
+        }
 
-		status.nof_retransmit++;
+        int recv_len = recv(socket, recv_packet, frame_size, 0);
+        if (recv_len < 0) {
+            perror("recv");
+            OutputDebugString("Receive failed\n");
+            return status;
+        }
+        else if (recv_len < HEADER_SIZE) {  // Incomplete transmission
+            nof_failures++;
+            OutputDebugString("Incomplete transmission\n");
+            Sleep(rand() % (1 << nof_failures) * slot_time); // Exponential backoff
+            continue;
+        }
 
-		//check if sent
+        packet_header_t* recv_header = (packet_header_t*)recv_packet;
 
-		fd_set readfds;
-		FD_ZERO(&readfds);
-		FD_SET(socket, &readfds);
+        //check for errors in
+        if (recv_header->type == PACKET_TYPE_NOISE) {
+            nof_failures++;
+            OutputDebugString("Received noise packet\n");
+            Sleep(rand() % (1 << nof_failures) * slot_time); // Exponential backoff
+            continue;
+        }
 
-		struct timeval tv;
-		tv.tv_sec = timeout;
-		tv.tv_usec = 0;
+        // Check if our packet
+        if (recv_header->type == PACKET_TYPE_DATA &&
+            ntohl(recv_header->seq_num) == seq_num &&
+            recv_header->src_ip == chan_addr->sin_addr.s_addr &&
+            recv_header->src_port == chan_addr->sin_port) {
+            // Success
+            QueryPerformanceCounter(&end_time);
+            status.success = 1;
+            OutputDebugString("Packet acknowledged\n");
+            return status;
+        }
+        else {
+            nof_failures++;
+            OutputDebugString("Received incorrect packet\n");
+            Sleep(rand() % (1 << nof_failures) * slot_time); // Exponential backoff
+            continue;
+        }
+    }
 
-		int select_resp = select(socket + 1, &readfds, NULL, NULL, &tv);
-		//error
-		if (select_resp < 0) {
-			perror("select");
-			return status;
-
-		}
-		//timeout
-		else if (select_resp == 0){
-			nof_failures++;
-			
-			Sleep(1000 * rand() % (1 << nof_failures) * slot_time); //exp backoff
-			continue;
-			
-		}
-
-		//wait for ack 
-
-		char* recv_packet = (char*)malloc(frame_size);
-		if (recv_packet == NULL) {  // Corrected check here
-			perror("Failed to allocate memory for recv_packet");
-			exit(1);
-		}
-
-		int recv_len = recv(socket, recv_packet, frame_size, 0);
-
-		if (recv_len < 0) {
-			perror("recv");
-			return status;
-		}
-		else if (recv_len < HEADER_SIZE) {  // Incomplete transmission
-			nof_failures++;
-			Sleep(1000 * rand() % (1 << nof_failures) * slot_time); // Exponential backoff
-			continue;
-		}
-
-		packet_header_t* recv_header = (packet_header_t*)recv_packet;
-
-		//check for errors in 
-
-		if (recv_header->type == PACKET_TYPE_NOISE)
-		{
-			nof_failures++;
-			Sleep(1000 * rand() % (1 << nof_failures) * slot_time); // Exponential backoff
-			continue;
-		}
-
-		// Check if our packet
-		if (
-			recv_header->type == PACKET_TYPE_DATA &&
-			ntohl(recv_header->seq_num) == seq_num &&
-			recv_header->src_ip == chan_addr->sin_addr.s_addr &&
-			recv_header->src_port == chan_addr->sin_port) {
-			// Success
-			QueryPerformanceCounter(&end_time);
-			status.success = 1;
-			return status;
-		}
-		else {
-			nof_failures++;
-			Sleep(1000 * rand() % (1 << nof_failures) * slot_time); // Exponential backoff
-			continue;
-
-		}
-		//To many attempts
-		return status;
-	}
-
-
-
+    //Too many attempts
+    OutputDebugString("Too many attempts, giving up\n");
+    return status;
 }
 
 int main(int argc, char* argv[]) {
+    Sleep(2000); //Remove 
+    //Check number of params
+    if (argc != 8) {
+        fprintf(stderr, "Not enough parameters - need 8");
+        OutputDebugString("Not enough parameters\n");
+        return 1;
+    }
 
-	//Check number of params
-	if (argc != 8) {
-		fprintf(stderr, "Not enough parametes - need 8");
-		return 1;
-	}
+    //assign params
+    const char* chan_ip = argv[1];
+    int chan_port = atoi(argv[2]);
+    const char* file_name = argv[3];
+    int frame_size = atoi(argv[4]);
+    int slot_time = atoi(argv[5]);
+    int seed = atoi(argv[6]);
+    int timeout = atoi(argv[7]);
 
-	//assign params
-	const char* chan_ip = argv[1];
-	int chan_port = atoi(argv[2]);
-	const char* file_name = argv[3];
-	int frame_size = atoi(argv[4]);
-	int slot_time = atoi(argv[5]);
-	int seed = atoi(argv[6]);
-	int timeout = atoi(argv[7]);
+    //read file
+    FILE* file = fopen(file_name, "rb");
+    if (!file) {
+        perror("Can't open file");
+        OutputDebugString("Can't open file\n");
+        return 1;
+    }
 
-	//read file 
-	FILE* file = fopen(file_name, "rb");
-	if (!file) {
-		perror("cant open file");
-		return 1;
-	}
-	// get file size 
-	fseek(file, 0, SEEK_END);
-	long file_size = ftell(file);
-	rewind(file);
+    // get file size
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    rewind(file);
 
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        fprintf(stderr, "WSAStartup failed.\n");
+        OutputDebugString("WSAStartup failed\n");
+        return 1;
+    }
 
-	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		fprintf(stderr, "WSAStartup failed.\n");
-		return 1;
-	}
-	
-	// Create socket
-	int channel_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (channel_socket < 0) {
-		perror("socket");
-		fclose(file);
-		return 1;
-	}
+    // Create socket
+    int channel_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (channel_socket < 0) {
+        perror("socket");
+        fclose(file);
+        OutputDebugString("Socket creation failed\n");
+        return 1;
+    }
 
+    printf("Socket created successfully.\n");
+    OutputDebugString("Socket created successfully\n");
 
-	struct sockaddr_in chan_addr;
-	memset(&chan_addr, 0, sizeof(chan_addr));
-	chan_addr.sin_family = AF_INET;
-	chan_addr.sin_port = htons(chan_port);
+    struct sockaddr_in chan_addr;
+    memset(&chan_addr, 0, sizeof(chan_addr));
+    chan_addr.sin_family = AF_INET;
+    chan_addr.sin_port = htons(chan_port);
 
-	if (inet_pton(AF_INET, chan_ip, &chan_addr.sin_addr) <= 0) {
-		perror("inet_pton failed");
-		return 1;
-	}
+    if (inet_pton(AF_INET, chan_ip, &chan_addr.sin_addr) <= 0) {
+        perror("inet_pton failed");
+        OutputDebugString("inet_pton failed\n");
+        return 1;
+    }
+    printf("IP address converted successfully.\n");
+    // Connect to channel
+    if (connect(channel_socket, (struct sockaddr*)&chan_addr, sizeof(chan_addr)) < 0) {
+        perror("connect");
+        closesocket(channel_socket);
+        fclose(file);
+        OutputDebugString("Connection failed\n");
+        return 1;
+    }
+    printf("Connected to channel.\n");
+    int payload_size = frame_size - HEADER_SIZE;
+    int total_frames = file_size / payload_size + 1; //round up
 
-	
+    //data transferring
+    char* payload = (char*)malloc(payload_size);
+    if (!payload) {
+        perror("malloc");
+        closesocket(channel_socket);
+        fclose(file);
+        OutputDebugString("Memory allocation failed\n");
+        return 1;
+    }
 
-	// Connect to channel
-	if (connect(channel_socket, (struct sockaddr*)&chan_addr, sizeof(chan_addr)) < 0) {
-		perror("connect");
-		closesocket(channel_socket);
-		fclose(file);
-		return 1;
-	}
+    //time start of the packet
+    LARGE_INTEGER start_time, end_time, freq;
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&start_time);
 
-	int payload_size = frame_size - HEADER_SIZE;
-	int total_frames = file_size / payload_size + 1; //round up
+    int seq_num = 0;
+    int success = 1;  // Overall success/failure flag
 
-	//data transfering
+    while (!feof(file)) {
+        printf("Sending frame %d\n", seq_num);
+        size_t read_chunk = fread(payload, 1, payload_size, file);
+        if (read_chunk == 0) break;
 
-	char* payload = (char*) malloc(payload_size);
-	if (!payload) {
-		perror("malloc");
-		closesocket(channel_socket);
-		fclose(file);
-		return 1;
-	}
-	//time start of the packet
-	LARGE_INTEGER start_time, end_time, freq;
-	QueryPerformanceFrequency(&freq);
-	QueryPerformanceCounter(&start_time);
+        send_st_t send_st = send_func(channel_socket, payload, HEADER_SIZE + read_chunk, seq_num, slot_time, timeout, &chan_addr);
+        printf("Send status: %d, retransmissions: %d\n", send_st.success, send_st.nof_retransmit);
+        if (!send_st.success) {
+            success = 0;
+            break;
+        }
 
-	
+        //Statistics
+        total_bytes_sent += read_chunk;
+        total_transmissions += send_st.nof_retransmit;
 
-	int seq_num = 0;
-	int success = 1;  // Overall success/failure flag
+        seq_num++;
+    }
 
-	while (!feof(file))
-	{
-		size_t read_chunk = fread(payload, 1, payload_size, file);
-		if (read_chunk == 0) break;
-		
-		send_st_t send_st = send_func(channel_socket, payload, HEADER_SIZE + read_chunk, seq_num, slot_time, timeout, &chan_addr);
+    QueryPerformanceCounter(&end_time);
 
-		if (!send_st.success) {
-			success = 0;
-			break;
-		}
+    //Calc time in ms
+    double total_time_ms = (double)(end_time.QuadPart - start_time.QuadPart) * 1000.0 / freq.QuadPart;
 
-		//Statistics 
-		total_bytes_sent += read_chunk;
-		total_transmissions += send_st.nof_retransmit;
-		
-		seq_num++;
+    //Calc BandWidth
+    double bandwidth = 0;
+    if (total_time_ms > 0) {
+        bandwidth = (total_bytes_sent * 8.0) / (total_time_ms / 1000.0) / 1000000.0;
+    }
 
-		
+    // Display statistics
+    fprintf(stderr, "Sent file %s\n", file_name);
+    fprintf(stderr, "Result: %s\n", success ? "Success :)" : "Failure :(");
+    fprintf(stderr, "File size: %llu Bytes (%d frames)\n", total_bytes_sent, total_frames);
+    fprintf(stderr, "Total transfer time: %.2f milliseconds\n", total_time_ms);
 
-	}
-	QueryPerformanceCounter(&end_time);
+    double avg_transmissions = 0;
+    if (total_frames > 0) {
+        avg_transmissions = (double)total_transmissions / total_frames;
+    }
 
-	//Calc time in ms
-	double total_time_ms = (double)(end_time.QuadPart - start_time.QuadPart) * 1000.0 / freq.QuadPart;
+    fprintf(stderr, "Transmissions/frame: average %.2f, maximum %d\n",
+        avg_transmissions, max_transmissions);
+    fprintf(stderr, "Average bandwidth: %.3f Mbps\n", bandwidth);
 
-	//Calc BandWidth
-	double bandwidth = 0;
-	if (total_time_ms > 0)
-	{
-		bandwidth = (total_bytes_sent * 8.0) / (total_time_ms / 1000.0) / 1000000.0;
-	}
-	// Display statistics
-	fprintf(stderr, "Sent file %s\n", file_name);
-	fprintf(stderr, "Result: %s\n", success ? "Success :)" : "Failure :(");
-	fprintf(stderr, "File size: %llu Bytes (%d frames)\n", total_bytes_sent, total_frames);
-	fprintf(stderr, "Total transfer time: %llu milliseconds\n", total_time_ms);
+    // Clean up
+    free(payload);
+    closesocket(channel_socket);
+    fclose(file);
 
-
-	double avg_transmissions = 0;
-	if (total_frames > 0) {
-		avg_transmissions = (double)total_transmissions / total_frames;
-	}
-
-	fprintf(stderr, "Transmissions/frame: average %.2f, maximum %d\n",
-		avg_transmissions, max_transmissions);
-	fprintf(stderr, "Average bandwidth: %.3f Mbps\n", bandwidth);
-
-	// Clean up
-	free(payload);
-	closesocket(channel_socket);
-	fclose(file);
-
-	return 0;
-
-
+    return 0;
 }
